f2f4a4b546090456b9f245ba04ca1ce53fe25742
Rjoydip <joydipand@gmail.com>
2017-10-04 23:04:19 -0800

    chore(): example 2

--- main.py
+++ main.py
@@ -3,11 +3,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-resp = requests.get("https://encrypted.google.com/async/flights_origin_destination?async=origin:CCU,destination:BLR,departure_date:2017-10-14,return_date:2017-10-20,is_oneway:false")
+####### Example 1
 
-content = resp.content
-soup = BeautifulSoup(content, "html.parser")
+# resp = requests.get("https://encrypted.google.com/async/flights_origin_destination?async=origin:CCU,destination:BLR,departure_date:2017-10-14,return_date:2017-10-20,is_oneway:false")
+
+# content = resp.content
+# soup = BeautifulSoup(content, "html.parser")
 
 # for s in soup:
-#     print(s[2])
+#  print(s[2])
 
+# print("style" in soup)
+
+
+############################################
+
+### Example 2
+
+rBlog = requests.get("https://rubygarage.org/blog")
+rBlogContent = rBlog.content
+
+rBlogContentSoup = BeautifulSoup(rBlogContent, "html.parser")
+
+print(rBlogContentSoup.prettify())
